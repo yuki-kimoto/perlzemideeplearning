abb5a542612e9279bace7f257a96183c73f5833f
Yuki Kimoto <kimoto.yuki@gmail.com>
2020-09-10 14:15:27 +0900

    rebuild

--- a.pl
+++ a.pl
@@ -13,9 +13,9 @@ sub sum_of_square_error {
   for (my $i = 0; $i < @$outputs; $i++) {
     $total_pow2 += ($outputs->[$i] - $desired_outputs->[$i]) ** 2;
   }
-  my $mean_square_error = 0.5 * $total_pow2;
+  my $sum_of_square_error = 0.5 * $total_pow2;
   
-  return $mean_square_error;
+  return $sum_of_square_error;
 }
 
 my $outputs = [0.7, 0.2, 0.1];
@@ -23,7 +23,6 @@ my $desired_outputs = [1, 0, 0];
 my $sum_of_square_error = sum_of_square_error($outputs, $desired_outputs);
 
 print "$sum_of_square_error\n";
-
 
 __END__
 my $ret = sigmoid(3);
