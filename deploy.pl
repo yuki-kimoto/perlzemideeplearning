64b951e7f5163d165637e18efaf64cf7518e1dc9
Yuki Kimoto <kimoto.yuki@gmail.com>
2021-12-23 15:02:24 +0900

    デプロイスクリプトを修正

--- deploy.pl
+++ deploy.pl
@@ -4,7 +4,7 @@ use utf8;
 
 my @args = @ARGV;
 
-my $deploy_cmd = q(ssh prod_perl_club_sites 'git -C ~/www/deeplearning-public fetch && git -C ~/www/deeplearning-public reset --hard origin/master');
+my $deploy_cmd = q(ssh prod_perl_club_sites 'git -C ~/www/perlzemideeplearning-public fetch && git -C ~/www/perlzemideeplearning-public reset --hard origin/master');
 
 system($deploy_cmd) == 0
   or die "Can't execute deploy command: $deploy_cmd:$!";
